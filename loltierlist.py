42a48881485f2863c722188f35f62b0e124a06fa
Niko <63622373+Nykoh@users.noreply.github.com>
2023-04-26 19:45:03 -0700

    Update loltierlist.py

--- loltierlist.py
+++ loltierlist.py
@@ -25,10 +25,21 @@ wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "div.rt-tr-grou
 
 champions = []
 win_rates = []
-pick_rates = []
+ban_rates = []
 test_1 = []
 champion_grades = []
-writer.writerow(['Champion Name', 'Win Rate', 'Ban Rate', 'Champion Grade'])
+matches_played = []
+test_2 = []
+test_7 = []
+test_8 = []
+test_9 = []
+test_10 = []
+test_11 = []
+test_12 = []
+test_13 = []
+test_14 = []
+test_15 = []
+writer.writerow(['Champion Name', 'Win Rate', 'Ban Rate', 'Champion Grade', 'Matches Played This Patch'])
 
 while True:
     # Scroll to the bottom of the page
@@ -49,28 +60,41 @@ while True:
         row = rows[i]
         champion = row.find_element(By.CSS_SELECTOR, "div.rt-td:nth-of-type(3)").get_attribute("textContent")
         win_rate = row.find_element(By.CSS_SELECTOR, "div.rt-td:nth-of-type(5)").text.strip()
-        pick_rate = row.find_element(By.CSS_SELECTOR, "div.rt-td:nth-of-type(6)").text.strip()
+        ban_rate = row.find_element(By.CSS_SELECTOR, "div.rt-td:nth-of-type(6)").text.strip()
         test1 = row.find_element(By.CSS_SELECTOR, "div.rt-td:nth-of-type(1)").get_attribute("textContent")
-        test2 = row.find_element(By.CSS_SELECTOR, "div.rt-td:nth-of-type(2)").get_attribute("textContent")
+        test2 = row.find_element(By.CSS_SELECTOR, "div.rt-td:nth-of-type(2)").text.strip() #fix this shit to show role position
         champion_grade = row.find_element(By.CSS_SELECTOR, "div.rt-td:nth-of-type(4)").text.strip()
-        test7 = row.find_element(By.CSS_SELECTOR, "div.rt-td:nth-of-type(7)").text.strip()
+        test7 = row.find_element(By.CSS_SELECTOR, "div.rt-td:nth-of-type(7)").text.strip() #this should be pick rate
+        match_played = row.find_element(By.CSS_SELECTOR, "div.rt-td:nth-of-type(8)").get_attribute("textContent")
+
             
         champions.append(champion)
         win_rates.append(win_rate)
-        pick_rates.append(pick_rate)
+        ban_rates.append(ban_rate)
         test_1.append(test1)
+        #test_2.append(test2)
         champion_grades.append(champion_grade)
+        test_7.append(test7)
+        matches_played.append(match_played)
 
-        writer.writerow([champion, win_rate, pick_rate, champion_grade])
+
+        writer.writerow([champion, win_rate, ban_rate, champion_grade, match_played])
 
 #print(champions)
 #print(win_rates)
-#print(pick_rates)
+#print(ban_rates)
 print(test1)
 print(test2)
 print(test7)
-print("-----")
+print("test 1")
 print(test_1)
+print("test 2")
+print(test_2)
+print("test 7")
+print(test_7)
+print("test 8")
+print(matches_played)
+
 
 driver.quit()
 
